69f298d55f0ac823af6f8e9b78c865590c048b07
= <hmgaar@gmail.com>
2016-10-08 06:23:27 -0400

    added PUT, PATCH to headers

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module TodoAngrails
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :header]
       end
     end
   end

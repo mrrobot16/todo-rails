332df695dbbc92ed86875b067f26f33c1094f4c1
= <hmgaar@gmail.com>
2016-10-08 07:29:59 -0400

    added DELETE to headers

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module TodoAngrails
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :header]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :header, :delete]
       end
     end
   end
